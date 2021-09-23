f7d0f8bcba9f0f19c7fe845c992c1a28eba6d2d1
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-09-24 00:55:19 +1000

    created home page

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const path = require('path')
+const homeRouter = require("./routes/home")
+const bodyParser  = require('body-parser'); 
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require("jquery")(window);
+require('dotenv').config();
 
 
+//init app
 const app = express();
 
-// engine
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static("public"));
+
+// app.set('views', path.join(__dirname, 'views'));
+
+//fetch data from the request
+app.use(bodyParser.urlencoded({extended:false}));
+
+//set template engine
 app.set('view engine', 'ejs')
 
+//set the path of the jquery file to be used from the node_module jquery package
+app.use('/jquery', express.static(path.join(__dirname + '/node_modules/jquery/dist/')));
 
+//set static folder(public) path 
+app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-
-
+app.use("/", homeRouter)
 
 PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
