e09b486e753ca8a9ed29c6d19d63802fd46d8c0a
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-09-26 22:49:24 +1000

    implemented search feature

--- public/data.js
+++ public/data.js
@@ -1,48 +1,57 @@
-const api_key = `api_key=c820500c0a92520524ea977cc56c8a32`
-const api_url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&${api_key}`;
+const api_key = `api_key=c820500c0a92520524ea977cc56c8a32`;
+const base_url = `https://api.themoviedb.org/3`;
+const api_url = `${base_url}/discover/movie?sort_by=popularity.desc&${api_key}`;
+const search_url = `${base_url}/search/movie?${api_key}`;
 
-fetchMovies(api_url)
+fetchMovies(api_url);
 
-function fetchMovies(url){
+function fetchMovies(url) {
   $.ajax({
     method: "GET",
     url: url,
     dataType: "json",
-  }).done((data) => {
-    const movies = data.results;
-    displayMovies(movies)
   })
-} 
-
-function displayMovies(movies){
- $(movies).each((i, val) => {
-    let parentDiv = $("<div class='card'>").addClass("card");
-    let moviePoster = $("<img class='card-img-top'>").attr({
-      src: "https://image.tmdb.org/t/p/w500" + val.poster_path,
-      alt: "poster",
+    .then((data) => {
+      const movies = data.results;
+      console.log(movies[0]);
+      displayMovies(movies);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-    let textDiv = $("<div class='card-body'>")
-      .html(`<p class='card-text'><a href=${val.id}>${val.title}</a>`)
-      
-    let rating = $("<p class='card-text'>")
-      .css("text-align", "center")
-      .text("Rating: ");
-    rating.append(val.vote_average);
-    moviePoster.appendTo(parentDiv);
-    parentDiv.append(textDiv, rating);
-    $(".box").append(parentDiv);
-  });
 }
 
 $("#searchForm").on("submit", (e) => {
   e.preventDefault();
   let searchText = $("#searchText").val();
-  
-  getMovie(searchText);
-  
-  console.log($("#searchForm"));
-})
+  if (searchText) {
+    fetchMovies(`${search_url}&query=${searchText}`);
+  } else {
+    fetchMovies(api_url);
+  }
+});
 
-function getMovie(searchText) {
-  console.log(searchText);
+// function getMovie(name) {
+//   fetchMovies(`${search_url}&query=${name}`)
+// }
+
+function displayMovies(movies) {
+  $(movies).each((i, val) => {
+    let parentDiv = $("<div class='card'>").addClass("card");
+    let moviePoster = $(
+      `<img class='card-img-top' src="https://image.tmdb.org/t/p/w500${val.poster_path}" alt="movie poster">`
+    );
+    let textDiv = $(
+      `<div class='card-body'><h6 class='card-title text-center'><a class=' nav-link text-info' href=${val.id}>${val.title}</a></h6></div>`
+    );
+    let rating = $(
+      "<p class='card-text'><span class='badge bg-info text-dark'>TMdb</span> </p>"
+    );
+    rating
+      .append(val.vote_average)
+      .append(`<p class="card-text d-inline float-end">${val.release_date.slice(0, 4)}`);
+    moviePoster.appendTo(parentDiv);
+    parentDiv.append(textDiv, rating);
+    $(".box").append(parentDiv);
+  });
 }
--- public/movie.js
+++ public/movie.js
@@ -3,9 +3,10 @@
 
 $.ajax(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=c820500c0a92520524ea977cc56c8a32`).then(data => {
     console.log(data);
-    let filmDis = $("<div class='card m-auto mb-3'>")
-    filmDis.append(`<img src="https://image.tmdb.org/t/p/w500${data.poster_path}" class="img-fluid">`)
-    filmDis.append(`<h2 class="card-title">${data.title}`)
-    filmDis.append(`<p class="card-text">${data.overview}`)
+    const details = [data.genres[0].name, data.original_language, data.production_companies[0].name, data.release_date, data.runtime, data.vote_average, data.tagline];
+    let filmDis = $("<div class='row'>")
+    filmDis.append(`<div class="col-md-4"><img src="https://image.tmdb.org/t/p/w500${data.poster_path}" class="img-fluid"></div>`)
+    filmDis.append(`<div class="col-md-8"><h2 class="card-title">${data.title}</h2><ul class="list-group"><li class="list-group-item bg-secondary text-info"><strong>Genre: ${details[0]}<li class="list-group-item bg-secondary text-info"><strong>Language: ${details[1]}<li class="list-group-item bg-secondary text-info"><strong>Production: ${details[2]}<li class="list-group-item bg-secondary text-info"><strong>Release Date: ${details[3]}<li class="list-group-item bg-secondary text-info"><strong>Duration: ${details[4]} Min<li class="list-group-item bg-secondary text-info"><strong>Rating: ${details[5]}<li class="list-group-item bg-secondary text-info"><strong>Tagline: ${details[6]}</ul>`)
+    filmDis.append(`<p class="card-text mt-2">${data.overview}`)
     $("#movie_view").append(filmDis)
 })
