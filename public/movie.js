7198df34d5e3e9c3ad386910891bc99d062e3313
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-09-29 21:18:23 +1000

    filter option

--- public/movie.js
+++ public/movie.js
@@ -6,7 +6,11 @@ $.ajax(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=c820500c0a9252052
     const details = [data.genres[0].name, data.original_language, data.production_companies[0].name, data.release_date, data.runtime, data.vote_average, data.tagline];
     let filmDis = $("<div class='row'>")
     filmDis.append(`<div class="col-md-4"><img src="https://image.tmdb.org/t/p/w500${data.poster_path}" class="img-fluid"></div>`)
-    filmDis.append(`<div class="col-md-8"><h2 class="card-title">${data.title}</h2><ul class="list-group"><li class="list-group-item bg-secondary text-info"><strong>Genre: ${details[0]}<li class="list-group-item bg-secondary text-info"><strong>Language: ${details[1]}<li class="list-group-item bg-secondary text-info"><strong>Production: ${details[2]}<li class="list-group-item bg-secondary text-info"><strong>Release Date: ${details[3]}<li class="list-group-item bg-secondary text-info"><strong>Duration: ${details[4]} Min<li class="list-group-item bg-secondary text-info"><strong>Rating: ${details[5]}<li class="list-group-item bg-secondary text-info"><strong>Tagline: ${details[6]}</ul>`)
+    filmDis.append(`<div class="col-md-8"><h2 class="card-title">${data.title}</h2><ul class="list-group"><li class="list-group-item bg-secondary text-info"><strong>Genre: ${details[0]}<li class="list-group-item bg-secondary text-info"><strong>Language: ${details[1]}<li class="list-group-item bg-secondary text-info"><strong>Production: ${details[2]}<li class="list-group-item bg-secondary text-info"><strong>Release Date: ${details[3]}<li class="list-group-item bg-secondary text-info"><strong>Duration: ${details[4]} Min<li class="list-group-item bg-secondary text-info"><strong>Rating: ${details[5]} <button class="btn btn-info" id="rate-btn">Rate</li><li class="list-group-item bg-secondary text-info"><strong>Tagline: ${details[6]}</ul>`)
     filmDis.append(`<p class="card-text mt-2">${data.overview}`)
     $("#movie_view").append(filmDis)
+})
+
+$("#rate-btn").on("click", () => {
+    console.log("clicked");
 })
